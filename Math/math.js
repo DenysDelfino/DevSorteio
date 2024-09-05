cf7acb75e75e3dfc8678822e09e8775327bd468f
Denys Lacerda Delfino <denyslacerda1990@gmail.com>
2024-09-05 18:40:39 -0300

    First Commit

---
+++ Math/math.js
@@ -0,0 +1,14 @@
+/*
+    Math. = Matemática
+    - pow(2,2) / potência
+    - sqrt(25,2) / raiz quadrada
+    - PI
+    - ceil / teto -> arredondar pra cima
+    - floor / chão -> arredondar pra baixo
+
+    - random() / número aleatório entre 0 e 1
+*/
+
+const result = Math.floor(3.14)
+
+console.log(result)
